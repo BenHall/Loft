fa0e37e5f04e636e3fa048dc3d0385c127f535d1
BenHall <ben@benhall.me.uk>
2010-04-22 00:12:20 +0100

    Tests around JsonRequester using FootReST

--- src/Loft/Loft.Specs/JsonRequesterSpecs.cs
+++ src/Loft/Loft.Specs/JsonRequesterSpecs.cs
@@ -1,7 +1,42 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
 namespace Loft.Specs
 {
-    public class JsonRequesterSpecs
+    public class JsonRequesterSpecs : IDisposable
     {
-        
+        FootReST.Server stubServer;
+
+        public JsonRequesterSpecs()
+        {
+            stubServer = new FootReST.Server();
+            stubServer.Start();
+        }
+
+        public void Dispose()
+        {
+            stubServer.Close();
+        }
+
+        [Fact]
+        public void Can_send_GET_request_to_server()
+        {
+            stubServer.DefineCustomResponse("GET", "/", "{\"ok\": true}");   
+
+            JsonRequester requester = new JsonRequester();
+            JObject jObject = requester.Get(new Server("localhost", 5984), "/");
+            Assert.True(jObject["ok"].Value<bool>());
+        }
+
+        [Fact]
+        public void Can_send_PUT_request_to_server()
+        {
+            stubServer.DefineCustomResponse("PUT", "/", "{\"ok\": true}");
+
+            JsonRequester requester = new JsonRequester();
+            JObject jObject = requester.Put(new Server("localhost", 5984), "/", "");
+            Assert.True(jObject["ok"].Value<bool>());
+        }
     }
 }
